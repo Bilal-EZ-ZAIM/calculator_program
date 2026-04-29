c24b5982ea11c3e1ee3eaa756c1b56b31f3d20c5
unknown <bilalzaim499@gmail.com>
2026-04-29 23:53:29 +0100

    add sum

--- Program.cs
+++ Program.cs
@@ -13,6 +13,19 @@
 
 // + - / * 
 
+
+// int x = 5;
+
+// 5  5+1 6 6+1 7 7+1 8 8+1 9 9+1 10
+
+
+// while (x < 10)
+// {
+//     Console.WriteLine("X rah As4ar mn 10");
+//     Console.WriteLine("X = " + x);
+//     x++;
+// }
+
 Console.WriteLine("Welcome to the calculator program!");
 Console.WriteLine("======= Calculator ========");
 Console.WriteLine("===================================");
@@ -21,45 +34,68 @@ Console.WriteLine("1. Addition (+)");
 Console.WriteLine("2. Subtraction (-)");
 Console.WriteLine("3. Multiplication (*)");
 Console.WriteLine("4. Division (/)");
+Console.WriteLine("0 . Exit");
 int operation;
-Console.WriteLine("Enter the operation you want to perform");
-operation = int.Parse(Console.ReadLine());
 double x;
 double y;
 double result;
-
-switch (operation)
+do
 {
-    case 1:
-        Console.WriteLine("Enter the first number");
-        x = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the second number");
-        y = double.Parse(Console.ReadLine());
-        result = x + y;
-        Console.WriteLine("Result: = " + result);
-        break;
-    case 2:
-        Console.WriteLine("Enter the first number");
-        x = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the second number");
-        y = double.Parse(Console.ReadLine());
-        result = x - y;
-        Console.WriteLine("Result: = " + result);
-        break;
-    case 3:
-        Console.WriteLine("Enter the first number");
-        x = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the second number");
-        y = double.Parse(Console.ReadLine());
-        result = x * y;
-        Console.WriteLine("Result: = " + result);
-        break;
-    case 4:
-        Console.WriteLine("Enter the first number");
-        x = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the second number");
-        y = double.Parse(Console.ReadLine());
-        result = x / y;
-        Console.WriteLine("Result: = " + result);
-        break;
+
+    Console.WriteLine("Enter the operation you want to perform");
+    operation = int.Parse(Console.ReadLine());
+    switch (operation)
+    {
+        case 1:
+            Console.WriteLine("Result: = " + sum());
+            break;
+        case 2:
+            Console.WriteLine("Enter the first number");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            y = double.Parse(Console.ReadLine());
+            result = x - y;
+            Console.WriteLine("Result: = " + result);
+            break;
+        case 3:
+            Console.WriteLine("Enter the first number");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            y = double.Parse(Console.ReadLine());
+            result = x * y;
+            Console.WriteLine("Result: = " + result);
+            break;
+        case 4:
+            Console.WriteLine("Enter the first number");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            y = double.Parse(Console.ReadLine());
+            result = x / y;
+            Console.WriteLine("Result: = " + result);
+            break;
+    }
+} while (operation != 0);
+
+double sum()
+{
+    Console.WriteLine("Enter the first number");
+    x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the second number");
+    y = double.Parse(Console.ReadLine());
+    result = x + y;
+    return result;
 }
+// type name(){
+// return type
+// }
+
+// int number()
+// {
+//     Console.WriteLine("Enter a number");
+//     int x = int.Parse(Console.ReadLine());
+//     return x;
+// }
+
+// Console.WriteLine("The number you entered is: " + number());
+
+
--- obj/Debug/net8.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac7eb3ef4bd8a68c5ae914087480a980586acd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
